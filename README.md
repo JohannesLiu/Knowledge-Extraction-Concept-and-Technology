16501a040040f702c421f24f0dac4d630a5633db
Xiaokai Liu <JohannesLiu@live.com>
2022-09-26 00:35:07 -0500

    udpate

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Knowledge-Extraction-Concept-and-Technology
+## 作者：刘晓凯
--- deploy.bat
+++
@@ -1,6 +0,0 @@
-gitbook build
-chdir D:\GitBookProjects\Knowledge-Extraction-Concept-and-Technology\_book
-git add -A
-git commit -m "update"
-git push origin master
-chdir ..
